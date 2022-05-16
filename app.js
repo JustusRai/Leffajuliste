6517b9c83b4692190920ceab5dd59fc49774bf40
JustusRai <99721716+JustusRai@users.noreply.github.com>
2022-05-16 21:37:17 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+const apinOsoite = 'https://api.themoviedb.org/3/movie/550?api_key=64e742f1948eb880020743c9e79e451a';
+const kuvienOsoite = "https://image.tmdb.org/t/p/w1280";
+const Etsi =
+    "https://api.themoviedb.org/3/search/movie?&api_key=64e742f1948eb880020743c9e79e451a&query=";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+haeElokuvat(apinOsoite);
+function haeElokuvat(url){
+    fetch(url).then(res => res.json())
+    .then(function(data){
+    
+    data.results.forEach(element => {
+        const alapalkki = document.createElement('div');
+        const image = document.createElement('img');
+        const text = document.createElement('h2');
+
+        text.innerHTML = `${element.title}`;
+        image.src = kuvienOsoite + element.poster_path;
+        alapalkki.appendChild(image);
+        alapalkki.appendChild(text);
+        main.appendChild(alapalkki);
+    }); 
+});
+}
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    main.innerHTML = '';
+     
+    const searchTerm = search.value;
+
+    if (searchTerm) {
+        haeElokuvat(Etsi + searchTerm);
+        search.value = "";
+    }
+});
